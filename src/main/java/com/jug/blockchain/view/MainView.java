a630a2041caae4f5fe779b7f787128dd54c31224
Ricardo Enrique Cantillo Carrillo <48037613+recantilloSena@users.noreply.github.com>
2020-04-24 22:53:31 -0500

    add POST films

---
+++ src/main/java/com/jug/blockchain/view/MainView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jug.blockchain.view;
+
+/**
+ *
+ * @author RICARDO
+ */
+public class MainView {
+    
+}
